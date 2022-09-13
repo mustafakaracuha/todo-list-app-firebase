3ed4aabdd7b3c75e3032a6e44271f8d7eeea76c6
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2022-09-13 17:42:49 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./assets/scss/todoList.scss";
 import TodoList from "./components/TodoList";
 import { ToastContainer} from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import firebase from 'firebase/app'
 import 'firebase/auth'
 
 const AppTitle = "To do List";
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
 import "./index.css";
 import App from "./App";
 import store from "./stores";
-import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
